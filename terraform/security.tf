d6e89b14c85724400826455991b5cbc72117250e
azfarsyafiq <azfarsyafiq13@gmail.com>
2026-09-04 00:12:03 +0800

    Add terraform infrastructure setup

--- terraform/security.tf
+++ terraform/security.tf
@@ -4,7 +4,7 @@
 
 resource "aws_security_group" "devops-public-sg" {
   name   = "devops-public-sg"
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = module.my_vpc.vpc_id 
 
   tags = {
     Name = "devops-public-sg"
@@ -21,21 +21,11 @@ resource "aws_vpc_security_group_ingress_rule" "public_http" {
   to_port     = 80
 }
 
-/* # Port 9100 from monitoring server
-resource "aws_vpc_security_group_ingress_rule" "public_monitoring" {
-  security_group_id = aws_security_group.devops-public-sg.id
-
-  cidr_ipv4   = "MONITORING_SERVER_IP/32"
-  ip_protocol = "tcp"
-  from_port   = 9100
-  to_port     = 9100
-} */
-
 # Port 22 from VPC subnet
 resource "aws_vpc_security_group_ingress_rule" "public_ssh" {
   security_group_id = aws_security_group.devops-public-sg.id
 
-  cidr_ipv4   = aws_vpc.my_vpc.cidr_block
+  cidr_ipv4   = module.my_vpc.vpc_cidr_block 
   ip_protocol = "tcp"
   from_port   = 22
   to_port     = 22
@@ -56,18 +46,34 @@ resource "aws_vpc_security_group_egress_rule" "public_outbound" {
 
 resource "aws_security_group" "devops-private-sg" {
   name   = "devops-private-sg"
-  vpc_id = aws_vpc.my_vpc.id
-
+  vpc_id = module.my_vpc.vpc_id 
   tags = {
     Name = "devops-private-sg"
   }
 }
 
+ /* # Port 9100 from monitoring server
+
+resource "aws_vpc_security_group_ingress_rule" "public_monitoring" {
+
+  security_group_id = aws_security_group.devops-public-sg.id
+
+
+  cidr_ipv4   = "MONITORING_SERVER_IP/32"
+
+  ip_protocol = "tcp"
+
+  from_port   = 9100
+
+  to_port     = 9100
+
+} */ 
+
 # Port 22 from VPC subnet
 resource "aws_vpc_security_group_ingress_rule" "private_ssh" {
   security_group_id = aws_security_group.devops-private-sg.id
 
-  cidr_ipv4   = aws_vpc.my_vpc.cidr_block
+  cidr_ipv4   = module.my_vpc.vpc_cidr_block # Fixed: was module.my_vpc.my_vpc.cidr_block
   ip_protocol = "tcp"
   from_port   = 22
   to_port     = 22
