26a8e7f49888a7d72b2e433565e8b0b2175ef40e
azfarsyafiq <azfarsyafiq13@gmail.com>
2026-09-13 22:30:58 +0800

    fix VPC name

--- terraform/network.tf
+++ terraform/network.tf
@@ -2,7 +2,7 @@ module "my_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 6.0"
 
-  name = "tf4-vpc"
+  name = "devops-vpc"
   cidr = "10.0.0.0/24"
   azs  = ["ap-southeast-1a"]
 
